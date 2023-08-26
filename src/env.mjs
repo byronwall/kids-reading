bb2dcfebe6ab1160495b5ba821aa3eefe41a64ac
Byron Wall <byron@byroni.us>
2023-08-26 12:41:46 -0400

    Add credential auth and jwt strategy

--- src/env.mjs
+++ src/env.mjs
@@ -20,6 +20,7 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
+    JWT_SECRET: z.string().min(1),
   },
 
   /**
@@ -40,6 +41,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,11 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
 import { GetServerSidePropsContext } from "next";
 import { DefaultSession, getServerSession, NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
@@ -17,11 +14,6 @@ declare module "next-auth" {
       // role: UserRole;
     };
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
@@ -31,16 +23,79 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    session({ session, user, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        // session.user.role = user.role; <-- put other properties on the session here
+      }
+      return session;
+    },
+    jwt({ token, user }) {
+      // Add auth_time to token on signin in
+      if (user !== undefined) {
+        token.auth_time = Math.floor(Date.now() / 1000);
+        token.id = user.id;
+      }
+      return Promise.resolve(token);
+    },
+  },
+  jwt: {
+    secret: env.JWT_SECRET,
   },
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "a@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials || !credentials.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        console.log("authorize called", { credentials, user });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        // check the passwords
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        console.log("passwordsMatch", passwordsMatch);
+
+        if (passwordsMatch) {
+          console.log("passwords match returning user", user);
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 };
 
 /**
