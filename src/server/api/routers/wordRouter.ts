c2599af9a86b912e344714c81ed228f5883bfaed
Byron Wall <byron@byroni.us>
2023-09-02 15:14:30 -0400

    Add word management New tRPC calls and an updated admin page

---
+++ src/server/api/routers/wordRouter.ts
@@ -0,0 +1,79 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { prisma } from "~/server/db";
+
+export const wordRouter = createTRPCRouter({
+  getAllWords: publicProcedure.query(async () => {
+    const words = await prisma.word.findMany({
+      orderBy: {
+        word: "asc",
+      },
+    });
+
+    return words;
+  }),
+
+  addWords: protectedProcedure.input(z.string()).mutation(async ({ input }) => {
+    const words = input.split(/,|\n/).map((word) => word.trim());
+
+    console.log("addWords mutation called", { words });
+
+    // check if word already exists
+    const existingWords = await prisma.word.findMany({
+      where: {
+        word: {
+          in: words,
+        },
+      },
+    });
+
+    const existingWordsSet = new Set(existingWords.map((word) => word.word));
+
+    const newWords = words.filter((word) => !existingWordsSet.has(word));
+
+    // create words
+    await prisma.word.createMany({
+      data: newWords.map((word) => ({
+        word,
+        metaInfo: {}, // add metaInfo property
+      })),
+    });
+
+    return {
+      message: `Words added successfully!`,
+    };
+  }),
+
+  deleteWord: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ input }) => {
+      const wordId = input;
+
+      // check if word exists
+      const word = await prisma.word.findUnique({
+        where: {
+          id: wordId,
+        },
+      });
+
+      if (!word) {
+        throw new Error(`Word with ID ${wordId} not found`);
+      }
+
+      // delete word
+      await prisma.word.delete({
+        where: {
+          id: wordId,
+        },
+      });
+
+      return {
+        message: `Word with ID ${wordId} deleted successfully!`,
+      };
+    }),
+});
