1c16b8991b320b509288596700461e89e53f1dd8
Byron Wall <byron@byroni.us>
2023-09-02 19:32:59 -0400

    Add basic profile management Create a user page to manage all user stuff

---
+++ src/app/user/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+
+import { trpc } from "../_trpc/client";
+
+export default function UserPage() {
+  const utils = trpc.useContext();
+
+  const [userName, setUserName] = useState("");
+
+  const handleUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const addProfileMutation = trpc.userRouter.addProfile.useMutation();
+
+  const { data: allProfiles } = trpc.userRouter.getAllProfiles.useQuery();
+
+  const handleAddProfile = async () => {
+    console.log(`User name is ${userName}`);
+
+    await addProfileMutation.mutateAsync({
+      profileName: userName,
+    });
+
+    await utils.userRouter.getAllProfiles.invalidate();
+  };
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Manage Profiles</CardTitle>
+          <CardDescription>
+            Profiles allow a user to manage multiple learners.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <section>
+            <form className="flex items-center gap-2">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="profileName">Add profile name</Label>
+                <Input
+                  type="profileName"
+                  id="profileName"
+                  placeholder="ProfileName"
+                  value={userName}
+                  onChange={handleUserNameChange}
+                />
+              </div>
+              <Button
+                type="button"
+                onClick={handleAddProfile}
+                className="shrink-0"
+              >
+                Add Profile
+              </Button>
+            </form>
+          </section>
+          <section className="mt-8">
+            <h3 className="mb-2 text-lg font-medium">Profiles</h3>
+            <ul className="divide-y divide-gray-200 rounded-md border border-gray-200">
+              {allProfiles?.map((profile) => (
+                <li
+                  key={profile.id}
+                  className="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
+                >
+                  <div className="flex w-0 flex-1 items-center">
+                    <span className="ml-2 w-0 flex-1 truncate">
+                      {profile.name}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </CardContent>
+      </Card>
+
+      <h2>Manage Account</h2>
+      {/* Add account management code here */}
+
+      <h2>Change Password</h2>
+      {/* Add password change code here */}
+    </div>
+  );
+}
--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -39,14 +39,9 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
         </div>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/user">Manage</Link>
         </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/dashboard/billing">Billing</Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/dashboard/settings">Settings</Link>
-        </DropdownMenuItem>
+
         <DropdownMenuSeparator />
         <DropdownMenuItem
           className="cursor-pointer"
