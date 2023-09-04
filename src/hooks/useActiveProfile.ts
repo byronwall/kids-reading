e18d4783780a82f1651ec54fb2941c014dbc15bf
Byron Wall <byron@byroni.us>
2023-09-04 09:42:10 -0400

    Invalidate all tRPC queries when profile changes

--- src/hooks/useActiveProfile.ts
+++ src/hooks/useActiveProfile.ts
@@ -1,13 +1,20 @@
 import { type Profile } from "@prisma/client";
 import { useSession } from "next-auth/react";
 
+import { trpc } from "~/app/_trpc/client";
+
 export function useActiveProfile() {
   const { data: session, update } = useSession();
+
+  const utils = trpc.useContext();
 
   const handleSetActiveProfile = async (profile: Profile) => {
     await update({
       activeProfile: profile,
     });
+
+    // ensure that all profile specific data is re-queried
+    await utils.invalidate();
   };
 
   return {
