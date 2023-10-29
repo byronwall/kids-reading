9ab66afc5070e0e4129e8dfbf19f224929640be6
Byron Wall <byron@byroni.us>
2023-10-28 22:38:55 -0400

    simplify word page

--- src/app/words/[word]/page.tsx
+++ src/app/words/[word]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from "next/navigation";
 
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { getSyllables } from "~/lib/splitArpabet";
 
 export default function WordPage() {
@@ -15,120 +16,24 @@ export default function WordPage() {
 
   const { syllables, stressLevels, arpabet } = getSyllables(word);
 
-  const phonicsInfo = [
-    { label: "Syllables", value: "2" },
-    { label: "Phonemes", value: "3" },
-    { label: "Examples", value: "Physics, Tonic, Sonic" },
-  ];
-
-  const wordInfo = {
-    title: "Word: Cat",
-    pronunciation: "Phonetic Pronunciation: /kæt/",
-    definition:
-      "A small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws.",
-    examples: [
-      "The cat sat on the mat.",
-      "I fed my cat some tuna.",
-      "The cat chased the mouse.",
-    ],
-    relatedPhonics: [
-      "The 'c' in 'cat' makes the /k/ sound.",
-      "The 'a' in 'cat' makes the /æ/ sound.",
-      "The 't' in 'cat' makes the /t/ sound.",
-    ],
-  };
-
-  const renderIcon = () => (
-    <svg
-      className="mr-2 inline-block h-4 w-4"
-      fill="none"
-      height="24"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-      width="24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <polyline points="20 6 9 17 4 12" />
-    </svg>
-  );
-
-  // Define utility classes
-  const sectionStyles = "py-12 md:py-24 lg:py-32";
-  const titleStyles = "mb-2 text-3xl font-semibold";
-  const textStyles = "text-lg text-gray-700";
-
   return (
     <div className="container mx-auto max-w-2xl">
-      <h1>Word Page</h1>
-      <p>word: {word}</p>
-
-      <pre>
-        <code>
-          {JSON.stringify({ arpabet, syllables, stressLevels }, null, 2)}
-        </code>
-      </pre>
-
-      <section className={`${sectionStyles} px-4 md:px-6`}>
-        <div className="grid gap-6 lg:grid-cols-2 lg:gap-12">
-          <div className="flex flex-col space-y-4">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              Phonics
-            </h2>
-            <p className="max-w-[600px] text-zinc-500 dark:text-zinc-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Pronounced as /fɒnɪks/
-            </p>
-            <ul className="grid gap-2 py-4">
-              {phonicsInfo.map((info) => (
-                <li key={info.label}>
-                  {renderIcon()}
-                  {info.label}: {info.value}
-                </li>
-              ))}
-            </ul>
-            <p className="max-w-[600px] text-zinc-500 dark:text-zinc-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Phonics is a method of teaching reading and spelling that stresses
-              symbol-sound relationships.
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <section className="p-8">
-        <div className="mx-auto max-w-3xl">
-          <h1 className={`${titleStyles} text-4xl`}>{wordInfo.title}</h1>
-          <p className="mb-4 text-lg font-semibold text-gray-500">
-            {wordInfo.pronunciation}
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>{word}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            <strong>Arpabet:</strong> {arpabet}
           </p>
-          <hr className="mb-6" />
-          <div className="mb-8">
-            <h2 className={titleStyles}>Definition:</h2>
-            <p className={textStyles}>{wordInfo.definition}</p>
-          </div>
-          <div className="mb-8">
-            <h2 className={titleStyles}>Examples:</h2>
-            <ul className="ml-5 list-disc">
-              {wordInfo.examples.map((example, index) => (
-                <li key={index} className={textStyles}>
-                  {example}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="mb-8">
-            <h2 className={titleStyles}>Related Phonics Rules:</h2>
-            <ul className="ml-5 list-disc">
-              {wordInfo.relatedPhonics.map((rule, index) => (
-                <li key={index} className={textStyles}>
-                  {rule}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </section>
+          <p>
+            <strong>Syllables:</strong> {syllables.join(" · ")}
+          </p>
+          <p>
+            <strong>Stress levels:</strong> {stressLevels.join(" · ")}
+          </p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
